9e750b94e5922001a2e2c9f361970fd981fdd189
tokodev <pablo@asdloop.com>
2023-11-22 12:56:30 +0100

    Add search feature

--- packages/nextjs/pages/products.tsx
+++ packages/nextjs/pages/products.tsx
@@ -8,6 +8,7 @@ const Products = () => {
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
   const [selectedBrands, setSelectedBrands] = useState<string[]>([]);
   const [filteredProductList, setFilteredProductList] = useState<Product[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
   const addCategory = (category: string) => {
     if (!selectedCategories.includes(category)) {
@@ -43,15 +44,36 @@ const Products = () => {
     setSelectedBrands([]);
   };
 
+  const handleSearch = () => {
+    // Filter based on search query and selected categories/brands
+    const filteredList = productList.filter(
+      item =>
+        (selectedCategories.length === 0 || selectedCategories.includes(item.category)) &&
+        (selectedBrands.length === 0 || selectedBrands.includes(item.brand)) &&
+        (item.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          item.description.toLowerCase().includes(searchQuery.toLowerCase())),
+    );
+
+    setFilteredProductList(filteredList);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   useEffect(() => {
-    if (selectedCategories.length === 0 && selectedBrands.length === 0) {
+    if (selectedCategories.length === 0 && selectedBrands.length === 0 && searchQuery === "") {
       setFilteredProductList(productList);
     } else {
       setFilteredProductList(
         productList.filter(
           item =>
             (selectedCategories.length === 0 || selectedCategories.includes(item.category)) &&
-            (selectedBrands.length === 0 || selectedBrands.includes(item.brand)),
+            (selectedBrands.length === 0 || selectedBrands.includes(item.brand)) &&
+            (item.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+              item.description.toLowerCase().includes(searchQuery.toLowerCase())),
         ),
       );
     }
@@ -81,6 +103,7 @@ const Products = () => {
       const updatedProducts: Product[] = data.products.map((product: Product) => ({
         ...product,
         brand: product.brand || "Unknown Brand",
+        description: product.description || "No description",
       }));
       setProductList(updatedProducts);
       setFilteredProductList(updatedProducts);
@@ -97,13 +120,27 @@ const Products = () => {
   }, []);
 
   return (
-    <div className="w-screen px-5 bg-gray-100 flex justify-center items-center">
+    <div className="px-5 bg-gray-100 flex justify-center items-center">
       <div className="w-full h-[90%] rounded-md bg-white">
+        {/* Search bar */}
+        <div className="p-6">
+          <input
+            type="text"
+            placeholder="Search Builds"
+            value={searchQuery}
+            onChange={e => setSearchQuery(e.target.value)}
+            onKeyDown={handleKeyPress} // Trigger search on 'Enter' key press
+            className="border rounded-md p-2"
+          />
+          <button onClick={handleSearch} className="bg-blue-500 text-white px-3 py-2 rounded-md ml-2">
+            Search
+          </button>
+        </div>
         <div className="relative w-full flex items-center overflow-x-auto">
           <div className="w-full flex-row items-start">
             {/* Category filter */}
-            <div className="flex flex-row mr-5 flex-wrap p-8">
-              <span className="mx-3 ml-5 font-medium"> Categories: </span>
+            <div className="flex flex-row mr-5 flex-wrap p-6">
+              <span className="font-medium mr-5"> Categories: </span>
               {categories.map(category => (
                 <div
                   onClick={() => {
@@ -113,7 +150,7 @@ const Products = () => {
                       addCategory(category);
                     }
                   }}
-                  className={`w-fit min-w-fit h-8 mx-2 px-5 py-2 flex flex-row justify-center items-center text-sm border break-keep rounded-3xl cursor-pointer transition-all duration-300 ${
+                  className={`w-fit min-w-fit h-6 mx-1 px-2 py-1 flex flex-row justify-center items-center text-sm border break-keep rounded-3xl cursor-pointer transition-all duration-300 text-xs ${
                     selectedCategories.includes(category)
                       ? "border-blue-500 bg-blue-500 text-white"
                       : " border-blue-500 bg-white text-gray-900"
@@ -129,13 +166,13 @@ const Products = () => {
                   selectedCategories.length > 0 ? "opacity-100" : "opacity-0 pointer-events-none"
                 } sticky right-0 w-fit h-full px-5 flex justify-center items-center text-blue-500 bg-white backdrop-blur-lg cursor-pointer hover:text-blue-700 transition-all duration-300`}
               >
-                clear
+                clear category filter
               </div>
             </div>
 
             {/* Brand filter */}
-            <div className="flex flex-row mr-5 flex-wrap p-8 gap-y-2">
-              <span className="mx-3 ml-5 font-medium"> Brands: </span>
+            <div className="flex flex-row mr-5 flex-wrap p-6 gap-y-2">
+              <span className="font-medium mr-5"> Brands: </span>
               {Array.from(new Set(productList.map(product => product.brand))).map(brand => (
                 <div
                   onClick={() => {
@@ -145,7 +182,7 @@ const Products = () => {
                       addBrand(brand);
                     }
                   }}
-                  className={`w-fit min-w-fit h-8 mx-2 px-5 py-2 flex flex-row justify-center items-center text-sm border break-keep rounded-3xl cursor-pointer transition-all duration-300 ${
+                  className={`w-fit min-w-fit h-6 mx-1 px-2 py-1 flex flex-row justify-center items-center text-sm border break-keep rounded-3xl cursor-pointer transition-all duration-300 text-xs ${
                     selectedBrands.includes(brand)
                       ? "border-red-500 bg-red-500 text-white"
                       : " border-red-500 bg-white text-gray-900"
@@ -161,12 +198,12 @@ const Products = () => {
                   selectedBrands.length > 0 ? "opacity-100" : "opacity-0 pointer-events-none"
                 } sticky right-0 w-fit h-full px-5 flex justify-center items-center text-blue-500 bg-white backdrop-blur-lg cursor-pointer hover:text-blue-700 transition-all duration-300`}
               >
-                clear
+                clear brand filter
               </div>
             </div>
           </div>
         </div>
-        <div className="p-8">
+        <div className="p-6">
           <ProductList filteredProductList={filteredProductList} loading={loading} />
         </div>
       </div>
